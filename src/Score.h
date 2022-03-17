5a8fbc0506ef5b73804f631f89fc89af51ae6047
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2022-03-16 22:11:59 -0700

    -

--- src/Score.h
+++ src/Score.h
@@ -58,7 +58,7 @@ public:
     Score(Harmony1Module *);
     void step() override;
     void draw(const DrawArgs &args) override;
- //   void drawLayer(const DrawArgs &args, int layer) override;
+    //   void drawLayer(const DrawArgs &args, int layer) override;
     bool isDirty() const override {
         return scoreIsDirty;
     }
@@ -93,6 +93,7 @@ private:
      * @return std::pair<float, bool> first is the y position, second it flag if need ledger line
      */
     YInfo noteYInfo(const MidiNote &note, bool bassStaff) const;
+    float noteY(const MidiNote &note, bool bassStaff) const;
 
     Harmony1Module *const module;
     std::list<Comp::Chord> chords;
@@ -104,18 +105,27 @@ private:
     const std::string gClef = u8"\ue050";
     const std::string fClef = u8"\ue062";
     const std::string ledgerLine = u8"\ue022";
+    const std::string flat = u8"\ue260";
+    const std::string sharp = u8"\ue262";
 
-    void drawStaff(const DrawArgs &args, float y);
-    void drawBarLine(const DrawArgs &args, float x, float y);
-    void drawChordInfo(const DrawArgs &args, float x, const Comp::Chord &chord);
-    void drawMusic(const DrawArgs &args);
-    void drawText(const DrawArgs &args);
-    void drawNotes(const DrawArgs &args);
+    void drawStaff(const DrawArgs &args, float y) const;
+    void drawBarLine(const DrawArgs &args, float x, float y) const;
+    void drawChordInfo(const DrawArgs &args, float x, const Comp::Chord &chord) const;
+    /**
+     * @return float width of key signature
+     */
+    float drawMusicNonNotes(const DrawArgs &args) const;
+    void drawChordNumbers(const DrawArgs &args, float widthOfKeysig) const;
+    void drawNotes(const DrawArgs &args, float widthOfKsig) const;
 
-    void drawKeysig(const DrawArgs& args, ConstScalePtr scale, bool trebleClef, float y);
+    /**
+     * @return float width of key signature
+     */
+    float drawKeysig(const DrawArgs &args, ConstScalePtr scale, bool trebleClef, float y) const;
+    float ledgerLine2Pos(int ledgerLine, bool bassStaff) const;
 
-    NVGcolor getForegroundColor();
-    NVGcolor getBackgroundColor();
+    NVGcolor getForegroundColor() const;
+    NVGcolor getBackgroundColor() const;
 
     // Y axis pos
     const float topMargin = 36.5f;
@@ -142,11 +152,11 @@ private:
     // const float barlineX2 = barlineX1 + 43;     // 40 too small 46 too big
 };
 
-NVGcolor Score::getForegroundColor() {
+NVGcolor Score::getForegroundColor() const {
     return whiteOnBlack ? nvgRGB(0xff, 0xff, 0xff) : nvgRGB(0, 0, 0);
 }
 
-NVGcolor Score::getBackgroundColor() {
+NVGcolor Score::getBackgroundColor() const {
     return whiteOnBlack ? nvgRGB(0, 0, 0) : nvgRGB(0xff, 0xff, 0xff);
 }
 
@@ -191,6 +201,15 @@ inline float Score::noteXPos(int noteNumber) const {
         x += deltaXNote;
     }
     return x;
+}
+
+float Score::noteY(const MidiNote &note, bool bassStaff) const {
+    float y = 0;
+    const float staffBasePos = bassStaff ? yBassStaff : yTrebleStaff;
+    const int ledgerLine = note.getLedgerLine(bassStaff);
+    y -= ledgerLine * spaceBetweenLines;
+    y += staffBasePos;
+    return y;
 }
 
 inline Score::YInfo Score::noteYInfo(const MidiNote &note, bool bassStaff) const {
@@ -256,14 +275,14 @@ inline void Score::drawLayer(const DrawArgs &args, int layer) {
 inline void Score::draw(const DrawArgs &args) {
     //  INFO("Score::draw");
     nvgScissor(args.vg, RECT_ARGS(args.clipBox));
-    drawMusic(args);
-    drawNotes(args);
-    drawText(args);
+    const float width = drawMusicNonNotes(args);
+    drawNotes(args, width);
+    drawChordNumbers(args, width);
     scoreIsDirty = false;
     Widget::draw(args);
 }
 
-inline void Score::drawText(const DrawArgs &args) {
+inline void Score::drawChordNumbers(const DrawArgs &args, float widthOfKeysig) const {
     if (chords.empty()) {
         return;
     }
@@ -276,12 +295,13 @@ inline void Score::drawText(const DrawArgs &args) {
     }
 }
 
-inline void Score::drawNotes(const DrawArgs &args) {
+inline void Score::drawNotes(const DrawArgs &args, float keysigWidth) const {
     if (!chords.empty()) {
         // INFO("chords to score %d", int(chords.size()));
         int i = 0;
         for (auto chord : chords) {
-            const float x = noteXPos(i);
+            SQINFO("x = %f w=%f", noteXPos(i), keysigWidth);
+            const float x = noteXPos(i) + keysigWidth;
 
             for (int i = 0; i < 4; ++i) {
                 const bool stemUp = i % 2;
@@ -302,17 +322,30 @@ inline void Score::drawNotes(const DrawArgs &args) {
     }
 }
 
-
-inline void Score:: drawKeysig(const DrawArgs &args, ConstScalePtr scale, bool trebble, float y) {
+inline float Score::drawKeysig(const DrawArgs &args, ConstScalePtr scale, bool trebble, float y) const {
+    const auto info = scale->getScoreInfo();
+    float width = 0;
+    if (info.numFlats) {
+        float w = 0; 
+        for (int i = 0; i < info.numFlats; ++i) {
+            const float x = xClef + 11 + w;
+            const auto note = info.flatsInTrebleClef[i];
+            const float yf = noteY(note, false);
+            nvgText(args.vg, x, yf, flat.c_str(), NULL);
+            w += 4;
+        }
+        width = std::max(width, w);
+    }
+    return width;
 }
 
-inline void Score::drawMusic(const DrawArgs &args) {
+inline float Score::drawMusicNonNotes(const DrawArgs &args) const {
     NVGcolor color = getBackgroundColor();
 
     // TODO : USE SIZE
-    //filledRect(args.vg, color, 0, 0, 200, 200, 5);
+    // filledRect(args.vg, color, 0, 0, 200, 200, 5);
     filledRect(args.vg, color, 0, 0, box.size.x, box.size.y, 5);
-    
+
     prepareFontMusic(args);
 
     color = getForegroundColor();
@@ -325,38 +358,14 @@ inline void Score::drawMusic(const DrawArgs &args) {
     drawStaff(args, yBassStaff);
     nvgText(args.vg, xClef, yBassClef, fClef.c_str(), NULL);
 
+    float width = 0;
     if (module) {
         auto scale = module->getScale();
-        drawKeysig(args, scale, true, yTrebleStaff);
-        drawKeysig(args, scale, false, yBassStaff);
+        const float a = drawKeysig(args, scale, true, yTrebleStaff);
+        const float b = drawKeysig(args, scale, false, yBassStaff);
+        width = std::max(width, a);
+        width = std::max(width, b);
     }
-
-    //   drawNotes(args);
-#if 0
-    if (!chords.empty()) {
-        // INFO("chords to score %d", int(chords.size()));
-        int i = 0;
-        for (auto chord : chords) {
-            const float x = noteXPos(i);
-
-            for (int i = 0; i < 4; ++i) {
-                const bool stemUp = i % 2;
-                auto yInfo = noteYInfo(chord.pitch[i], i < 2);
-
-                for (int i = 0; i < 3; ++i) {
-                    if (yInfo.ledgerPos[i] != 0) {
-                        // printf("drawing ledger at %f\n", yInfo.ledgerPos[i]);
-                        nvgText(args.vg, x, yInfo.ledgerPos[i], ledgerLine.c_str(), NULL);
-                    }
-                }
-                const char *note = stemUp ? noteQuarterUp.c_str() : noteQuarterDown.c_str();
-                // printf("drawing note at %f\n", yInfo.position);
-                nvgText(args.vg, x, yInfo.position, note, NULL);
-            }
-            ++i;
-        }
-    }
-#endif
 
     drawBarLine(args, xStaff, yBassStaff);
     drawBarLine(args, barlineX1, yBassStaff);
@@ -364,10 +373,12 @@ inline void Score::drawMusic(const DrawArgs &args) {
     const float barlineX2 = args.clipBox.size.x - leftMargin;
     drawBarLine(args, barlineX2, yBassStaff);
 
-    TransparentWidget::draw(args);
+// bgf: non const - didn't compile. do we want this here?
+  //  TransparentWidget::draw(args);
+    return width;
 }
 
-inline void Score::drawChordInfo(const DrawArgs &args, float x, const Comp::Chord &chord) {
+inline void Score::drawChordInfo(const DrawArgs &args, float x, const Comp::Chord &chord) const {
     {
         std::stringstream s;
         s << chord.root;
@@ -388,7 +399,7 @@ inline void Score::drawChordInfo(const DrawArgs &args, float x, const Comp::Chor
 #endif
 }
 
-inline void Score::drawStaff(const DrawArgs &args, float yBase) {
+inline void Score::drawStaff(const DrawArgs &args, float yBase) const {
     const float x = xStaff;
     const float length = args.clipBox.size.x - 2 * leftMargin;
     // auto color = nvgRGB(0xff, 0xff, 0xff);
@@ -399,7 +410,7 @@ inline void Score::drawStaff(const DrawArgs &args, float yBase) {
     }
 }
 
-inline void Score::drawBarLine(const DrawArgs &args, float x, float y) {
+inline void Score::drawBarLine(const DrawArgs &args, float x, float y) const {
     auto color = getForegroundColor();
     drawVLine(args.vg, color, x, y, barlineHeight, .5f);
 }
@@ -427,7 +438,7 @@ void Score::drawHLine(NVGcontext *vg, NVGcolor color, float x, float y, float le
 void Score::filledRect(NVGcontext *vg, NVGcolor color, float x, float y, float w, float h, float rounding) const {
     nvgFillColor(vg, color);
     nvgBeginPath(vg);
- //   nvgRect(vg, x, y, w, h);
+    //   nvgRect(vg, x, y, w, h);
     nvgRoundedRect(vg, x, y, w, h, rounding);
     nvgFill(vg);
 }
@@ -448,5 +459,5 @@ void Score::prepareFontMusic(const DrawArgs &args) const {
 void Score::prepareFontText(const DrawArgs &args) const {
     // int f = APP->window->uiFont->handle;
     nvgFontFaceId(args.vg, APP->window->uiFont->handle);
-    nvgFontSize(args.vg, 7);        // TODO: use our own font?
+    nvgFontSize(args.vg, 7);  // TODO: use our own font?
 }
