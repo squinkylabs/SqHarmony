285fa0e837c20db3e834cc2ec65e02e8c87befda
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2022-08-27 15:49:31 -0700

    -

--- composites/AdditivePitchLogic.h
+++ composites/AdditivePitchLogic.h
@@ -1,14 +1,33 @@
 #pragma once
 
+#include <cmath>
+#include "SqLog.h"
+
 template <unsigned N>
 class AdditivePitchLogic {
 public:
     /** returns the aggregate pitch in VCV standard volts.
      */
-    float getPitch(int harmonic);
+    float getPitch(int harmonic) const;
+    
+    // VC is VCV standard
+    void setCV(float cv);
+    void setOctave(int oct);
+    void setSemitone(int semi);
+
+    // 0 is no stretch, 1 is max, -1 is max "unstretch"
+    void setStretch(float stretch);
+
+    void setEvenOffset(float offset);
+    void setOddOffset(float offset);
+    void setOffset(float offset);
+
+
 };
 
 template <unsigned N>
-inline float AdditivePitchLogic<N>::getPitch(int harmonic) {
-    return 0;
+inline float AdditivePitchLogic<N>::getPitch(int harmonic) const {
+    const auto lgh = std::log2(float(harmonic+1));
+    SQINFO("harm=%d, log=%f", harmonic, lgh);
+    return lgh;
 }
--- tests/testAdditivePitchLogic.cpp
+++ tests/testAdditivePitchLogic.cpp
@@ -1,16 +1,51 @@
 
-#include "asserts.h"
 #include "AdditivePitchLogic.h"
+#include "asserts.h"
+
+template <unsigned N>
+inline  bool areAtHarmonics(const AdditivePitchLogic<N>& l) {
+    const float base = l.getPitch(0);
+    const float base2 = l.getPitch(1);
+    // 2 is oct and 5th
+    const float base4 = l.getPitch(3);
+
+
+   
+    if (base2 != (base +1)) {
+        SQINFO("base 2 = %f 2base=%f", base2, base + 1);
+        return false;
+    }
+
+    if (base4 != (base + 2)) {
+        SQINFO("base 4 = %f 4base=%f", base4, base + 2);
+        return false;
+    }
+
+    assert(false);
+
+    return true;
+}
 
 static void testAdditivePitchLogic_initialPitch() {
     const static unsigned N = 16;
     AdditivePitchLogic<N> l;
-    for (auto i = 0; i < N; ++i) {
-        assertEQ(l.getPitch(i), 0.f);
-    }
+    assertEQ(l.getPitch(0), 0.f);
+    assert(areAtHarmonics(l));
 }
 
+static void testAdditivePitchLogic_cv(float cv) {
+    assert(false);
+}
+
+static void testAdditivePitchLogic_cv() {
+    testAdditivePitchLogic_cv(1);
+    testAdditivePitchLogic_cv(2);
+    testAdditivePitchLogic_cv(-3);
+    testAdditivePitchLogic_cv(0.1f);
+    testAdditivePitchLogic_cv(-0.1f);
+}
 
 void testAdditivePitchLogic() {
     testAdditivePitchLogic_initialPitch();
+    testAdditivePitchLogic_cv();
 }
