688cf5cfe1936db334a221dd913098505032ce85
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2022-08-28 11:32:28 -0700

    -

--- composites/Additive.h
+++ composites/Additive.h
@@ -9,6 +9,31 @@ class Additive : public TBase {
 public:
     // Additive();
     Additive(Module *);
+
+    enum ParamIds {
+        SCHEMA_PARAM,
+        OCTAVE_PARAM,
+        SEMI_PARAM,  // how many notes we hold in the buffer
+        FINE_PARAM,  // how we play the pattern back
+        STRETCH_PARAM,
+        EVENOFFSET_PARAM,
+        ODDOFFSET_PARAM,
+        NUM_PARAMS
+    };
+
+    enum InputIds {
+        CV_INPUT,
+        NUM_INPUTS
+    };
+
+    enum OutputIds {
+        AUDIO_OUTPUT,
+        NUM_OUTPUTS
+    };
+
+    enum LightIds {
+        NUM_LIGHTS
+    };
 };
 
 template <class TBase>
--- src/Additive.cpp
+++ src/Additive.cpp
@@ -1,18 +1,35 @@
 #include "Additive.h"
+
 #include "WidgetComposite.h"
 #include "plugin.hpp"
 #include "rack.hpp"
-
 
 using Comp = Additive<WidgetComposite>;
 #define _LAB
 
 class AdditiveModule : public Module {
 public:
+    AdditiveModule() {
+        config(Comp::NUM_PARAMS, Comp::NUM_INPUTS, Comp::NUM_OUTPUTS, Comp::NUM_LIGHTS);
+        addParams();
+    }
+
+    void process(const ProcessArgs& args) override {
+        comp->process(args);
+    }
+
+private:
+    void addParams();
+
 private:
     std::shared_ptr<Comp> comp = std::make_shared<Comp>(this);
 };
 
+void AdditiveModule::addParams() {
+    this->configParam(Comp::SCHEMA_PARAM, 0, 10, 0, "Schema");  // invisible - just for the future.
+}
+
+//---------------------------------
 class AdditiveWidget : public ModuleWidget {
 public:
     AdditiveWidget(AdditiveModule*& module) {
@@ -21,14 +38,16 @@ public:
 #ifdef _LAB
         addLabel(Vec(20, 12), "Additive", 24);
 #endif
+        addControls();
     }
 
 private:
+    void addControls();
 #ifdef _LAB
     Label* addLabel(const Vec& v, const std::string& str, float fontSize = 14) {
         NVGcolor white = nvgRGB(0xe0, 0xe0, 0xe0);
         Label* label = new Label();
-        label->box.pos = v;
+        label->box.pos = v - 1.5f * str.size();
         label->text = str;
         label->color = white;
         label->fontSize = fontSize;
@@ -37,5 +56,28 @@ private:
     }
 #endif
 };
+
+Vec position(int row, int col) {
+    float x = col * 37 + 20;
+    float y = row * 30 + 50;
+    return Vec(x, y);
+}
+
+Vec labelPos(int row, int col) {
+    float x = col * 37 + 20;
+    float y = row * 30 + 30;
+    return Vec(x, y);
+}
+
+void AdditiveWidget::addControls() {
+    auto param = createParam<RoundBlackSnapKnob>(position(0,0), module, Comp::OCTAVE_PARAM);
+    addParam(param);
+    addLabel(labelPos(0,0), "oct");
+
+    param = createParam<RoundBlackSnapKnob>(position(0,1), module, Comp::SEMI_PARAM);
+    addParam(param);
+    addLabel(labelPos(0,1), "semi");
+
+}
 
 rack::Model* modelAdditive = createModel<AdditiveModule, AdditiveWidget>("sqh-additive");
