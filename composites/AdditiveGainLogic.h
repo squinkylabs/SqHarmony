171aaea88987d55141ed83bc30e64e781e4a7372
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2022-08-30 17:25:15 -0700

    -

--- composites/AdditiveGainLogic.h
+++ composites/AdditiveGainLogic.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include "AudioMath.h"
+#include "SqLog.h"
+#include <string>
 
 template <unsigned N>
 class AdditiveGainLogic {
 public:
+    AdditiveGainLogic();
+
     void setEven(float);
     void setOdd(float);
+
+    /**
+     * slope 1.f is flat
+     * slope 0 is really rolled off
+     */
     void setSlope(float);
     void setHarmonic(unsigned n, float value);
     //
@@ -13,7 +23,32 @@ public:
 
     const float defaultLevel = .8f;
 
+    void dump(const std::string& s) const;
+private:
+    float harmonicLevels[N];
+    float _slope = 1;
+   
 };
+
+template <unsigned N>
+inline AdditiveGainLogic<N>::AdditiveGainLogic() {
+    for (unsigned i=0; i<N; ++i) {
+        harmonicLevels[i] = defaultLevel;
+    }
+}
+
+template <unsigned N>
+inline void AdditiveGainLogic<N>::setHarmonic(unsigned harmonic, float value) {
+    if (harmonic >= N) {
+        return;
+    } 
+    harmonicLevels[harmonic] = value;
+}
+
+template <unsigned N>
+inline void AdditiveGainLogic<N>::setSlope(float value) {
+    _slope = value;
+}
 
 template <unsigned N>
 inline float AdditiveGainLogic<N>::getLevel(unsigned harmonic) const {
@@ -21,5 +56,20 @@ inline float AdditiveGainLogic<N>::getLevel(unsigned harmonic) const {
     if (harmonic >= N) {
         return 0;
     }
-    return defaultLevel;
+    float value = harmonicLevels[harmonic];
+
+    const float slopeAttenuationDB = harmonic * (1 - _slope);
+    const float slopeGain = (float) AudioMath::gainFromDb(-slopeAttenuationDB);
+   // SQINFO("slope=%f, harm=%d slopeGain =%f sadb=%f", _slope, harmonic, slopeGain, slopeAttenuationDB);
+    value *= slopeGain;
+    return value;
+}
+
+template <unsigned N>
+inline void AdditiveGainLogic<N>::dump(const std::string& s) const {
+    SQINFO("dumping AGL %s", s.c_str());
+    for (unsigned i=0; i<N; ++i) {
+        SQINFO("hlev[%d] = %f", i, harmonicLevels[i]);
+    }
+    SQINFO("slope=%f", _slope);
 }
--- tests/testAdditiveGainLogic.cpp
+++ tests/testAdditiveGainLogic.cpp
@@ -26,7 +26,33 @@ static void testAdditiveGainLogic_outOfRange() {
     */
 }
 
+static void testAdditiveGainLogic_harmonics() {
+    const static unsigned N = 16;
+    AdditiveGainLogic<N> l;
+    l.setHarmonic(0, 0);
+    assertEQ(l.getLevel(0), 0);
+    assertEQ(l.getLevel(1), l.defaultLevel);
+ }
+
+static void testAdditiveGainLogic_slope() {
+    const static unsigned N = 16;
+    AdditiveGainLogic<N> l;
+    l.setSlope(0);
+
+   // l.dump("after slope 0");
+    float last = l.defaultLevel;
+    assertEQ(l.getLevel(0), l.defaultLevel);
+    for (unsigned i=1; i<N; ++i) {
+      //  SQINFO("in test loop on %d level=%f", i, l.getLevel(i));
+        assertLT(l.getLevel(i), 1);
+        assertLT(l.getLevel(i), last);
+        last = l.getLevel(i);
+    }
+}
+
 void testAdditiveGainLogic() {
     testAdditiveGainLogic_initial();
     testAdditiveGainLogic_outOfRange();
+    testAdditiveGainLogic_harmonics();
+    testAdditiveGainLogic_slope();
 }
