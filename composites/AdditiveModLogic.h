d1e965ef6d6ff5a07f4a0f1a53ac25159d81ab4e
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2022-09-02 08:25:49 -0700

    -

--- composites/AdditiveModLogic.h
+++ composites/AdditiveModLogic.h
@@ -10,5 +10,13 @@ class AdditiveModLogic {
 public:
     void setADSRValue(unsigned adsr, float value);
     void setADSRTrimValue(unsigned adsr, unsigned harmonic, float value);
+    void setHarmonicBaseLevel(unsigned harmonic, float value);
+
+    float getCombinedADSR(unsigned harmonic) const;
   
 };
+
+template <unsigned N>
+inline float AdditiveModLogic<N>::getCombinedADSR(unsigned harmonic) const {
+    return 0;
+}
--- tests/testAdditiveModLogic.cpp
+++ tests/testAdditiveModLogic.cpp
@@ -2,10 +2,19 @@
 #include "AdditiveModLogic.h"
 #include "asserts.h"
 
-static void test0() {
-    AdditiveModLogic<16> l;
-
+template <unsigned N>
+static void assertAllOutputsZero(const AdditiveModLogic<N>& l) {
+    for (unsigned i = 0; i < N; ++i) {
+        const float x = l.getCombinedADSR(i);
+        assertEQ(x, 0);
+    }
 }
+
+static void testAdditiveModLogic_initial() {
+    AdditiveModLogic<16> l;
+    assertAllOutputsZero(l);
+}
+
 void testAdditiveModLogic() {
-    test0();
+    testAdditiveModLogic_initial();
 }
