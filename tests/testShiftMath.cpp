aa91ce14fb82c930b30cc22a591b6823d2feff72
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2023-10-20 22:49:51 -0700

    -

--- tests/testShiftMath.cpp
+++ tests/testShiftMath.cpp
@@ -2,10 +2,34 @@
 #include "asserts.h"
 #include "ShiftMath.h"
 
-static void testCanCall() {
-   // ShiftMath s;
+static void testConstruct() {
+    ClockWithPhase ck (12, .3);
+    assertEQ(ck._clocks, 12);
+    assertClose(ck._phase, .3, .00001);
+
+    ClockWithSamples x(14, 2345);
+    assertEQ(x._clocks, 14);
+    assertEQ(x._samples, 2345);
+}
+
+static void testConvert1() {
+    ClockWithPhase cph(0, .5);
+    const auto csp = convert(cph, 100);     // 100 cycle period
+
+    assertEQ(csp._clocks, 0);
+    assertEQ(csp._samples, 50);
+}
+
+static void testConvert2() {
+    ClockWithPhase cph(70, .9);
+    const auto csp = convert(cph, 1000);     // 100 cycle period
+
+    assertEQ(csp._clocks, 70);
+    assertEQ(csp._samples, 900);
 }
 
 void testShiftMath() {
-    testCanCall();
+    testConstruct();
+    testConvert1();
+    testConvert2();
 }
--- util/ShiftMath.h
+++ util/ShiftMath.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <cmath>
+#include "assert.h"
+
 class ClockWithPhase {
 public:
     ClockWithPhase() {}
-    ClockWithPhase(int clocks, float phase) : _clocks(clocks), _phase(phase) {}
+    ClockWithPhase(int clocks, float phase) : _clocks(clocks), _phase(phase) {
+        assert(_clocks >= 0);
+        assert(_phase >= 0);
+        assert(_phase <= 1);
+    }
 
     int _clocks = 0;
     float _phase = 0;
@@ -13,12 +20,15 @@ class ClockWithSamples {
 public:
     ClockWithSamples() {}
     ClockWithSamples(int clocks, int samples) : _clocks(clocks), _samples(samples) {}
-
     int _clocks = 0;
     int _samples = 0;
 };
 
-// class ShiftMath {
-//     static void clock
+inline ClockWithSamples convert(const ClockWithPhase& input, int periodOfClock) {
+    assert(periodOfClock > 0);
+    const int clocks = input._clocks;
+    const int samples = std::round(input._phase * double(periodOfClock));
+    return ClockWithSamples(clocks, samples);
+}
 
-// };
+
