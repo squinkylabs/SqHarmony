4cd700827b94355eb5db88320bbfed9917550b98
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2023-10-22 22:05:26 -0700

    -

--- tests/testShiftCalc.cpp
+++ tests/testShiftCalc.cpp
@@ -6,8 +6,26 @@ static void testCanCall() {
     ShiftCalc s;
     const float f = s.go();
     s.setup(0);
+    s.trigger(1);
+}
+
+static void testGeneratesNothing() {
+    ShiftCalc s;
+    s.setup(0);
+    const float f = s.go();
+    assertEQ(f, 0);
+}
+
+static void testGeneratesSomething() {
+    ShiftCalc s;
+    s.setup(0);
+    s.trigger(10);
+    const float f = s.go();
+    assertGT(f, 0);
 }
 
 void testShiftCalc() {
     testCanCall();
+    testGeneratesNothing();
+    testGeneratesSomething();
 }
--- util/ShiftCalc.h
+++ util/ShiftCalc.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <assert.h>
+
 class ShiftCalc {
 public:
     /**
@@ -9,12 +11,27 @@ public:
      */
     float go();
     void setup(int samplesInMasterPeriod);
+    void trigger(int periodOfClock);
 private:
+    double _acc = 0;
+    double _delta = 0;
 };
 
 inline float ShiftCalc::go() {
-    return 0;
+    if (_delta) {
+        _acc += _delta;
+        if (_acc >= 1) {
+            _acc = 1;
+            _delta = 0;
+        }
+    }
+    return float(_acc);
 }
 
 inline void ShiftCalc::setup(int samplesInMasterPeriod) {
+} 
+
+inline void ShiftCalc::trigger(int periodOfClock) {
+    assert(periodOfClock > 0);
+    _delta = 1.0 / double(periodOfClock);
 } 
