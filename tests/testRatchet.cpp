e444c25dc6dfc525e41ba5aba92ac53f6996cdf6
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2023-10-29 16:27:34 -0700

    -

--- tests/testRatchet.cpp
+++ tests/testRatchet.cpp
@@ -6,14 +6,35 @@
 
 static void testCanCall() {
     Ratchet r;
-    (void) r;
 
     r.setCount(4);
-    const bool b =  r.run(true);
+    const int i =  r.run(true);
     r.trigger();
+    r.onOutputClock();
+    bool b =  r.isStillRatcheting();
+}
+
+static void testNoTrigger() {
+    Ratchet r;
+    for (int i=0; i<10; ++i) {
+        const int b =  r.run(false);
+        assertEQ(r.run(false), 1);
+        const bool x = r.isStillRatcheting();
+        assertEQ(r.isStillRatcheting(), false);
+    }
+}
+
+static void testTrigger() {
+    Ratchet r;
+    r.setCount(2);
+    r.trigger();
+    int i = r.run(true);
+    assertEQ(i, 2);
+    assertEQ(r.isStillRatcheting(), true);
 }
 
 void testRatchet() {
     testCanCall();
-  
+    testNoTrigger();  
+    testTrigger();
 }
--- util/Ratchet.h
+++ util/Ratchet.h
@@ -2,19 +2,49 @@
 
 class Ratchet {
 public:
-    void setCount(float count);
-    bool run(bool ckIn);
+    void setCount(int count);
+    /**
+     * @brief when it's time to ratchet, return the count. Otherwise returns 1
+     * 
+     * @param ckIn 
+     * @return int 
+     */
+    int run(bool ckIn);
     void trigger();
+    //
+    void onOutputClock();
+    bool isStillRatcheting() const;
 private:
+    // What the caller passed to setCount.
+    int _count = 1;
+    bool _isTriggered = false;
 
+    // Each time a ratchet is triggered, this counts down.
+    int _countDown = 0;
 };
 
-inline void Ratchet::setCount(float count) {
+inline void Ratchet::setCount(int count) {
+    _count = count;
 }
 
-inline bool Ratchet::run(bool ckIn) {
-    return false;
+inline int Ratchet::run(bool ckIn) {
+    if (!_isTriggered) {
+        return 1;
+    }
+    // TODO: do we support factional or not??
+    int ret = int(_count);
+  //  _count = 1;
+    _countDown = _count;
+    return ret;
 }
 
 inline void Ratchet::trigger() {
+    _isTriggered = true;
+}
+
+inline void Ratchet::onOutputClock() {
+}
+
+inline bool Ratchet::isStillRatcheting() const {
+    return _countDown > 0;
 }
