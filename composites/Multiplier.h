6c8936e532786d62389dea95fd8ce7ea0ff34d84
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2023-10-31 08:11:19 -0700

    -

--- composites/Multiplier.h
+++ composites/Multiplier.h
@@ -63,6 +63,10 @@ private:
 
     GateTrigger _ratchet1Trigger;
     Ratchet _ratchet1;
+
+    // temporary hacks to get ratchet working.
+    bool _ratchetArmed = false;
+    int _ratchetCount = 1;
 };
 
 template <class TBase>
@@ -82,8 +86,12 @@ inline void Multiplier<TBase>::_updateMultAmount() {
 
 template <class TBase>
 inline void Multiplier<TBase>::_updateRatchet1Count() {
-    float count = TBase::params[RATCHET_1_COUNT_PARAM].value;
+    int count = int(std::round(TBase::params[RATCHET_1_COUNT_PARAM].value));
     _ratchet1.setCount(int(count));
+    if (count != _ratchetCount) {
+        _ratchetCount = count;
+        SQINFO("updated ratchet count to %d", count);
+    }
 }
 
 template <class TBase>
@@ -96,14 +104,57 @@ template <class TBase>
 inline void Multiplier<TBase>::process(const typename TBase::ProcessArgs& args) {
     divn.step();
 
-     {
+    // Look at the ratchet input. Arm if needed
+    {
+        const float rawRatchet1In = TBase::inputs[RATCHET_1_TRIGGER_INPUT].getVoltage();
+        const bool ratchet = rawRatchet1In > 5; // TODO, use schmidt
+        if (ratchet) {
+            _ratchetArmed = true;
+            SQINFO("ratchet armed");  
+        }
+    }
+    
+    // Look at the clock input, detect trigger
+    bool triggerIn = false;
+    {
+        const float rawClockIn = TBase::inputs[CK_INPUT].getVoltage();
+        _inputClockProc.go(rawClockIn);
+        triggerIn = _inputClockProc.trigger();
+    }
+
+    // Decide if new count needs to be jammed into mult.
+    if (triggerIn && _ratchetArmed) {
+        _clockMult.setMul(_ratchetCount);
+        _ratchetArmed = false;
+        SQINFO("set mult to %d", _ratchetCount);
+    }
+
+    // Now that mult is set up, we can process the clock multiplier
+    {
+        const bool rawClockOut = _clockMult.run(triggerIn);
+        if (rawClockOut) {
+            _triggerOutOneShot.set();
+        }
+        _triggerOutOneShot.step(args.sampleTime);
+        const bool trigger = !_triggerOutOneShot.hasFired();
+        const float clockOut = trigger ? cGateOutHi : cGateOutLow;
+        TBase::outputs[CK_OUTPUT].setVoltage(clockOut);
+    }
+}
+
+// old version
+#if 0
+template <class TBase>
+inline void Multiplier<TBase>::process(const typename TBase::ProcessArgs& args) {
+    divn.step();
+
+    {
         const float rawClockIn = TBase::inputs[CK_INPUT].getVoltage();
         _inputClockProc.go(rawClockIn);
         const bool triggerIn = _inputClockProc.trigger();
         const bool rawClockOut = _clockMult.run(triggerIn);
         if (rawClockOut) {
             _triggerOutOneShot.set();
-
         }
         _triggerOutOneShot.step(args.sampleTime);
         const bool trigger = !_triggerOutOneShot.hasFired();
@@ -126,8 +177,9 @@ inline void Multiplier<TBase>::process(const typename TBase::ProcessArgs& args) 
         const int count = _ratchet1.run(inputClockTrigger);
         if (count != 1) {
             SQINFO("got count %d, will set on mult", count);
-           // _ratchet1.setCount(count);
-           _clockMult.setMul(count);
+            // _ratchet1.setCount(count);
+            _clockMult.setMul(count);
         }
-    } 
+    }
 }
+#endif
