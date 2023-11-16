8a3ba79fdec92b5dce97c0c7a03f565f3e865855
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2023-11-15 21:33:52 -0800

    -

---
+++ util/FreqMeasure2.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+
+#include "SqLog.h"
+#include "assert.h"
+
+class FreqMeasure {
+public:
+  //  void onSample(bool trigger);
+    void process(bool trigger, bool gate);
+    bool freqValid() const;
+    int getPeriod() const;
+    int getHighDuration() const;
+
+private:
+    int _count = 0;
+    int _period = 0;
+    int _clocksReceived = 0;
+    bool _lastClock = false;
+};
+
+inline void FreqMeasure::onSample(bool clock) {
+
+    // only process transition, but being held still adds to period.
+    if (clock == _lastClock) {
+        ++_count;
+        return;
+    }
+    _lastClock = clock;
+
+    if (clock) {
+        _clocksReceived++;
+        if (_clocksReceived > 10) {
+            _clocksReceived = 10;
+        }
+        _period = _count;
+        _count = 0;
+    } else {
+        ++_count;
+    }
+}
+
+inline bool FreqMeasure::freqValid() const {
+    return _clocksReceived > 1;
+}
+
+inline int FreqMeasure::getPeriod() const {
+    assert(freqValid());
+    return _period + 1;
+}
