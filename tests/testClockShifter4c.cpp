83576528dee3b5b4ccd494bc123b4f5d131cb336
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2023-12-06 17:16:20 -0800

    -

--- tests/testClockShifter4c.cpp
+++ tests/testClockShifter4c.cpp
@@ -8,7 +8,35 @@
  * 
  */
 
+static void testSpeedUp() {
+    auto shifter = makeAndPrime(10, 0);
+    assertEQ(shifter->getPeriod(), 10);
+    bool b = clockItLow(shifter, 7);
+    assert(!b);
+    shifter->process(true, true);
+    assertEQ(shifter->getPeriod(), 8);
+}
+
+static void testSlowDown() {
+    auto shifter = makeAndPrime(8, 0);
+    assertEQ(shifter->getPeriod(), 8);
+    bool b = clockItLow(shifter, 19);
+    assert(!b);
+    shifter->process(true, true);
+    assertEQ(shifter->getPeriod(), 20);
+}
+
+static void testSpeedUpOutputsClock() {
+    auto shifter = makeAndPrime(10, 0);
+    assertEQ(shifter->getPeriod(), 10);
+    bool b = clockItLow(shifter, 7);
+    assert(!b);
+    b = shifter->process(true, true);
+    assert(b);
+}
 
 void testClockShifter4c() {
-    assert(false);
+    testSpeedUp();
+    testSlowDown();
+    testSpeedUpOutputsClock();
 }
