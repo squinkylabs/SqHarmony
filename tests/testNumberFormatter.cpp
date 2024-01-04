c16ff0c4d10b0f627860cc5fc61f358aa0602e21
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-01-03 18:10:03 -0800

    -

--- tests/testNumberFormatter.cpp
+++ tests/testNumberFormatter.cpp
@@ -7,6 +7,28 @@ static void test0() {
     auto s2 = NumberFormatter::formatFloat(2, s);
     assertEQ(s2, "123.45");
 }
+
+static void test1() {
+    std::string s("123.4");
+    auto s2 = NumberFormatter::formatFloat(2, s);
+    assertEQ(s2, "123.40");
+}
+
+static void test2() {
+    std::string s("123.45");
+    auto s2 = NumberFormatter::formatFloat(2, s);
+    assertEQ(s2, "123.45");
+}
+
+static void test3() {
+    std::string s("123");
+    auto s2 = NumberFormatter::formatFloat(2, s);
+    assertEQ(s2, "123.00");
+}
+
 void testNumberFormatter() {
     test0();
+    test1();
+    test2();
+    test3();
 }
--- util/NumberFormatter.h
+++ util/NumberFormatter.h
@@ -6,13 +6,23 @@ class NumberFormatter {
 public:
     static std::string formatFloat(int decimalPlaces, const std::string s) {
         auto pos = s.find('.');
-        assert(pos != std::string::npos);
+        if (pos == std::string::npos) {
+            return s + ".00";
+        }
+
         const int excess = s.length() - (pos + 3);
         SQINFO("excess =%d", excess);
         if (excess > 0) {
             return s.substr(0, s.length() - excess);
         }
-        assert(false);
-        return "";
+        if (excess < 0) {
+            auto sret = s;
+            for (int i = 0; i < -excess; ++i) {
+                //sret.pop_back();
+                sret.push_back('0');
+            }
+            return sret;
+        }
+        return s;
     }
 };
