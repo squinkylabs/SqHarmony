7f96a917ccf07a39c726d6bca2ddb6761c42a286
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-02-01 18:32:56 -0800

    -

--- tests/testNoiseGen.cpp
+++ tests/testNoiseGen.cpp
@@ -4,12 +4,12 @@
 #include "asserts.h"
 
 static void testCanCall() {
-    NoiseGen n;
+    NoiseGen n(0);
     n.get();
 }
 
 static void testDifferentValues() {
-    NoiseGen n;
+    NoiseGen n(0);
     float_4 x = n.get();
     float_4 y = n.get();
     for (int i=0; i<4; ++i) {
@@ -17,7 +17,25 @@ static void testDifferentValues() {
     }
 } 
 
+static void testChannelsDifferent() {
+    NoiseGen n(12);
+    float_4 x = n.get();
+     for (int i=1; i<4; ++i) {
+        assertNE(x[0], x[i]);
+     }
+}
+
+static void testDifferentSeeds() {
+    NoiseGen n(0);
+    NoiseGen n2(1);
+    assertNE(n.get()[0], n2.get()[0]);
+}
+
+
 void testNoiseGen() {
     testCanCall();
     testDifferentValues();
+    testChannelsDifferent();
+    testDifferentSeeds();
+
 }
--- util/dsp/NoiseGen.h
+++ util/dsp/NoiseGen.h
@@ -4,17 +4,23 @@
 
 class NoiseGen {
 public:
+    NoiseGen(int base);
     // generates a new random
     float_4 get();
 private:
     std::mt19937 gen32[4];
 };
 
+inline NoiseGen::NoiseGen(int base) {
+    for (int i=0; i<4; ++i) {
+        gen32[i].seed(i + base * 4);
+    }
+}
+
 inline float_4 NoiseGen::get() {
     float_4 ret;
     for (int i=0; i<4; ++i) {
         ret[i] = gen32[i]();
     }
-    //return {gen32[3]()};
     return ret;
 }
