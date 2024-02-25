103479a88de1e85067edf1576f4294da534ddba8
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-02-25 09:53:59 -0800

    -

--- tests/testClockShifter6.cpp
+++ tests/testClockShifter6.cpp
@@ -3,9 +3,24 @@
 
 static void testCanCreate() {
     ClockShifter6 c;
-    assert(false);
+    c.setMaxDelaySamples(1);
+    c.process(0, false);
+}
+
+static void canReturnSizeError() {
+    ClockShifter6 c;
+    c.setMaxDelaySamples(1);
+
+    ClockShifter6::Errors err;
+
+    c.process(false, 100, &err);
+    assert(err == ClockShifter6::Errors::ExceededDelaySize);
+
+    c.process(false, 1, &err);
+    assert(err == ClockShifter6::Errors::NoError);
 }
 
 void testClockShifter6() {
     testCanCreate();
+    canReturnSizeError();
 }
--- util/ClockShifter6.h
+++ util/ClockShifter6.h
@@ -6,12 +6,46 @@
 // #include "FreqMeasure2.h"
 // #include "OneShotSampleTimer.h"
 
+#include <cstdint>
+#include <vector>
+
+#include "SqLog.h"
+
 class ClockShifter6 {
 public:
     friend class TestX;
-    bool process(bool trigger, bool clock, float shift);
-   
+    enum class Errors {
+        NoError,
+        ExceededDelaySize,
+        LostClocks
+    };
+    bool process(bool clock, float delay, Errors* error = nullptr);
+    void setMaxDelaySamples(unsigned samples);
+
 private:
-   
+    // delay memory as 32 bit unsigned, but accesses as a bit.
+    std::vector<uint32_t> _delayMemory;
+
+    size_t _maxDelaySize();
 };
 
+size_t ClockShifter6::_maxDelaySize() {
+    return _delayMemory.size() * 32;
+}
+
+inline bool ClockShifter6::process(bool clock, float delay, Errors* error) {
+    SQINFO("in process, delay mem=%u", _delayMemory.size());
+    if (error) {
+        *error = Errors::NoError;
+
+        if (delay > _maxDelaySize()) {
+            *error = Errors::ExceededDelaySize;
+        }
+    }
+    return false;
+}
+
+inline void ClockShifter6::setMaxDelaySamples(unsigned samples) {
+
+    _delayMemory.resize((samples + 31) / 32);
+}
