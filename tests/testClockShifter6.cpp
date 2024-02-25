6ddd984473416e031ad6952875fb7f4ef39c838b
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-02-25 10:45:24 -0800

    -

--- tests/testClockShifter6.cpp
+++ tests/testClockShifter6.cpp
@@ -20,7 +20,41 @@ static void canReturnSizeError() {
     assert(err == ClockShifter6::Errors::NoError);
 }
 
+class TestX {
+public:
+    static void canDelayZero() {
+        ClockShifter6 c;
+        c.setMaxDelaySamples(0);
+        c._insertDelayInput(false);
+        bool x = c._getDelayOutput(0);
+        assertEQ(x, false);
+
+        c._insertDelayInput(true);
+        x = c._getDelayOutput(0);
+        assertEQ(x, true);
+    }
+    static void canExtractBit() {
+        assertEQ(ClockShifter6::_extractBit(0, 0), false);
+        assertEQ(ClockShifter6::_extractBit(1, 0), true);
+    }
+};
+
+static void canDelayZero() {
+    ClockShifter6 c;
+    c.setMaxDelaySamples(0);
+    ClockShifter6::Errors err;
+    bool x = c.process(true, 0, &err);
+    assertEQ(x, true);
+
+    x = c.process(false, 0, &err);
+    assertEQ(x, false);
+}
+
 void testClockShifter6() {
-    testCanCreate();
-    canReturnSizeError();
+    TestX::canExtractBit();
+  //  TestX::canDelayZero();
+
+   // testCanCreate();
+   // canReturnSizeError();
+   // canDelayZero();
 }
--- util/ClockShifter6.h
+++ util/ClockShifter6.h
@@ -1,7 +1,8 @@
 #pragma once
 
 // #include <cmath>
-// #include <tuple>
+#include <assert.h>
+#include <tuple>
 
 // #include "FreqMeasure2.h"
 // #include "OneShotSampleTimer.h"
@@ -27,10 +28,34 @@ private:
     std::vector<uint32_t> _delayMemory;
 
     size_t _maxDelaySize();
+    unsigned _delayInSamples=0;
+    unsigned _delayOutSamples=0;
+    bool _getDelayOutput(float delay);
+    void _insertDelayInput(bool data);
+
+    std::tuple<unsigned, unsigned> _getIndexAndBit(unsigned bitIndex);
+    static bool _extractBit(unsigned word, unsigned bit);
 };
 
 size_t ClockShifter6::_maxDelaySize() {
     return _delayMemory.size() * 32;
+}
+
+inline void ClockShifter6::_insertDelayInput(bool data) {
+    const auto indexAndBit = ClockShifter6::_getIndexAndBit(_delayInSamples);
+    assert(false);
+}
+
+inline std::tuple<unsigned, unsigned> ClockShifter6::_getIndexAndBit(unsigned bitIndex) {
+    unsigned index = _delayInSamples / 32;
+    unsigned bit = _delayInSamples - index;
+    return std::make_tuple(index, bit);
+}
+
+inline bool ClockShifter6::_extractBit(unsigned word, unsigned bit) {
+    unsigned shifted = word >> bit;
+    unsigned mask = 1 << bit;
+    return bool(shifted & mask);
 }
 
 inline bool ClockShifter6::process(bool clock, float delay, Errors* error) {
@@ -40,12 +65,24 @@ inline bool ClockShifter6::process(bool clock, float delay, Errors* error) {
 
         if (delay > _maxDelaySize()) {
             *error = Errors::ExceededDelaySize;
+            return false;
         }
     }
-    return false;
+
+    const bool outputClock = _getDelayOutput(delay);
+    _insertDelayInput(clock);
+    return outputClock;
 }
 
 inline void ClockShifter6::setMaxDelaySamples(unsigned samples) {
-
     _delayMemory.resize((samples + 31) / 32);
 }
+
+inline  bool ClockShifter6::_getDelayOutput(float delay) {
+    const auto indexAndBit =  _getIndexAndBit(_delayInSamples); 
+    const unsigned index = std::get<0>(indexAndBit);
+    const unsigned bit = std::get<1>(indexAndBit);
+    unsigned x = _delayMemory.at(index);
+    return _extractBit(x, bit);
+}
+
