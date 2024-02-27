2402f52c82e01c4f2d396258c6b6000bdf78a36f
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-02-27 08:29:54 -0800

    -

--- tests/main.cpp
+++ tests/main.cpp
@@ -93,7 +93,7 @@ int main(const char**, int) {
         SQINFO("doing special long run");
     }
 
-#if 1
+#if 0
     printf("running first for temp debug\n");
     assert(SqLog::errorCount == 0);
     testFirst();
--- tests/testClockShifter6d.cpp
+++ tests/testClockShifter6d.cpp
@@ -11,7 +11,6 @@
  */
 extern bool doLongRunning;
 
-
 class SignalSourceInterface {
 public:
     virtual bool getClock() const = 0;
@@ -21,57 +20,87 @@ public:
 };
 
 class SignalSource : public SignalSourceInterface {
- public:
-     bool getClock() const override {
-         return false;
-     }
-     float getDelay() const override {
-         return 0;
-     }
-     bool isMoreData() const override {
-         return false;
-     }
-     void next() override {
-         assert(false);
-     }
+public:
+    bool getClock() const override {
+        return false;
+    }
+    float getDelay() const override {
+        return 0;
+    }
+    bool isMoreData() const override {
+        return _samplesRemaining > 0;
+    }
+    void next() override {
+        assert(isMoreData());
+        _generateNextSample();
+    }
+
+    void initForDelayRamp(float initial, float final, unsigned duration) {
+        assert(initial != final);
+        _sampleCounter = 0;
+        _samplesRemaining = duration;
+
+        _currentDelay = initial;
+        _deltaDelayPerSample = (final - initial) / duration;
+        assert(_deltaDelayPerSample != 0);
+    }
 private:
+    double _currentDelay = 0;
+    unsigned _sampleCounter = 0;
+    int _samplesRemaining = 0;
+    double _deltaDelayPerSample = 0;
+
+    void _generateNextSample() {
+        _sampleCounter++;
+        _samplesRemaining--;
+        _currentDelay += _deltaDelayPerSample;
+    }
 };
 
+class Output6 {
+public:
+    int outputClocks = 0;
+    int minSamplesBetweenClocks = 100000;
+    int maxSamplesBetweenClocks = 0;
 
-static void testMakeSource() {
-    SignalSource s;
+    float maxDelay = -100;
+    float minDelay = 100;
+};
+
+static void run(SignalSourceInterface*, Output6* theResults) {
 }
 
+static void testCanCall() {
+    SignalSource s;
+    s.initForDelayRamp(0, 1, 100);
+    Output6 results;
+    run(&s, &results);
+}
 
+static void testDelayRamp() {
+    const unsigned dur = 100;
+    SignalSource s;
+    s.initForDelayRamp(0, 1, 100);
+    for (int i=0; i<100; ++i) {
+        assertEQ(s.isMoreData(), true);
+        s.next();
+    }
+    assertEQ(s.isMoreData(), false);
 
+}
 
 void testClockShifter6d() {
-    testMakeSource();
-    //  SQINFO("start testClockShifter5d");
+    testCanCall();
+    testDelayRamp();
     assertEQ(SqLog::errorCount, 0);
-   // test0();
-   // testNoShift();
-   // testNoShiftTwice();
-
-    SQINFO("why is test stop not working?");
-    // testStop();
-  //  testSlowDownOld();
-
-   // testSpeedUp();
-   // testSlowDownAndSpeedUp();
-
-   // testSpeedUp(5, 7872, -0.000063516257796437, 10);
-
-   // testWithLFO();
 }
 
 #if 1
 void testFirst() {
     ClockShifter6::llv = 1;
     //  This is the case that is bad without the dodgy "fix"
-   // testWithLFO(4, 16, 0.136364, 0.400000, 3);
+    // testWithLFO(4, 16, 0.136364, 0.400000, 3);
 
-   //testSlowDown(5, 3552, 0.0001407658, 7);
-
+    // testSlowDown(5, 3552, 0.0001407658, 7);
 }
 #endif
