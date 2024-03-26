fa9b68cf346c0c8dd028dac94c1ae8b4149460a4
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-03-25 21:12:01 -0700

    -

--- composites/CompositeUpdater.h
+++ composites/CompositeUpdater.h
@@ -5,30 +5,38 @@
 template <typename T>
 class ParamUpdater {
 public:
-    ParamUpdater(T* composite, int paramID);
+    ParamUpdater(T* composite, int paramID) : _composite(composite), _paramID(paramID) {}
     ParamUpdater() = delete;
-    ParamUpdater(const ParamUpdater&) = delete;
 
 private:
     T* const _composite;
+    const int _paramID;
 };
 
 template <typename T>
 class CVUpdater {
 public:
-    CVUpdater(T* composite, int paramID);
-    CVUpdater() = delete;
-    CVUpdater(const CVUpdater&) = delete;
+    CVUpdater(T* composite, int inputID) : _composite(composite), _inputID(inputID) { }
+
+   CVUpdater() = delete;
+  //  CVUpdater(const CVUpdater&) = delete;
 
 private:
     T* const _composite;
+    const int _inputID;
 };
 
 template <typename T>
 class CompositeUpdater {
 public:
-    CompositeUpdater(const std::vector<ParamUpdater<T>>&, const std::vector<CVUpdater<T>>& );
+    CompositeUpdater(std::vector<ParamUpdater<T>>&&, std::vector<CVUpdater<T>>&& );
 private:
     std::vector<ParamUpdater<T>> _paramUpdaters;
     std::vector<CVUpdater<T>> _cvUpdaters;
 };
+
+template <typename T>
+inline CompositeUpdater<T>::CompositeUpdater(
+    std::vector<ParamUpdater<T>>&& pu, 
+    std::vector<CVUpdater<T>>&& cu) : _cvUpdaters(std::move(cu)) {
+}
--- tests/testUpdater.cpp
+++ tests/testUpdater.cpp
@@ -24,10 +24,46 @@ static void testCanCall() {
     CompositeUpdater<Comp> c({}, {});
     CVUpdater<Comp> cv(&composite, Comp::PARAM1);
     ParamUpdater<Comp> param(&composite, Comp::INPUT1);
+
+    std::vector<CVUpdater<Comp>> v;
+    v.push_back({ &composite,  int(Comp::PARAM1) });
+    v.push_back({ &composite,  Comp::PARAM1 });
+
+    std::vector<ParamUpdater<Comp>> vpu;
+    vpu.push_back({&composite, 4});
+
+   std::vector<CVUpdater<Comp>> v2(std::move(v));
+
+    CompositeUpdater<Comp> c2(std::move(vpu), std::move(v));
+
+    CompositeUpdater<Comp> v3(
+        { {&composite, 2} },
+        { {&composite, 5} });
+
+}
+
+class foo {
+public:
+    std::vector<int> v;
+  foo(std::vector<int>&& x) : v(std::move(x)) {}
+};
+
+
+
+
+static void testExp() {
+
+    foo f = std::vector<int>{ 1,2,3 };
+
+    std::vector<int> g = std::vector<int>{ 1,2,3 };
+    SQINFO("f=%d g=%d", f.v.size(), g.size());
+    foo h(std::move(g));
+    SQINFO("h=%d g=%d", h.v.size(), g.size());
 }
 
 void testUpdater() {
-    assert(false);
+    testCanCall();
+    testExp();
 }
 
 #if 1
