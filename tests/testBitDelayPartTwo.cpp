50e7a053720bf172094e44b1cd8931e9e5ad1912
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-04-29 21:06:54 -0700

    -

--- tests/testBitDelayPartTwo.cpp
+++ tests/testBitDelayPartTwo.cpp
@@ -18,6 +18,7 @@ static void fillDelay(T& delay, const TestData& data) {
         const unsigned count = std::get<1>(d);
         for (unsigned j=0; j<count; ++j) {
             //   bool process(bool InputClock, unsigned delay, Errors* error = nullptr);
+            SQINFO("fill with %d", value);
             delay.process(value, 0);
         }
     }
@@ -28,18 +29,30 @@ static void validateDelay(T& delay, const TestData& data) {
     assert(!data.empty());
     const auto lastData = data[data.size() - 1];
 
-
+    unsigned totalCount = 1;
     for (size_t i = 0; i < data.size(); ++i) {
         const auto d = data[i];
         const bool value = std::get<0>(d);
         const unsigned count = std::get<1>(d);
         for (unsigned j = 0; j < count; ++j) {
             //   bool process(bool InputClock, unsigned delay, Errors* error = nullptr);
-            const bool delayedValue = delay.process(false, value);
+            const bool delayedValue = delay.process(false, totalCount);
 
-            SQINFO("delayed = %d i=%d, j=%d", delayedValue, i, j);
+            // assert delayed == d
+            assertEQ(delayedValue, value);
+
+            SQINFO("delayed = %d i=%d, j=%d totlcount=%d value = %d", delayedValue, i, j, totalCount, value);
+            totalCount += 2;    // we just processed one, and we just added one.s
         }
     }
+
+    for (int i = 0; i < 10; ++i) {
+        bool b = delay.process(false, totalCount);
+        SQINFO("after: b=%d tc=%d", b, totalCount);
+        assertEQ(b, false);
+        totalCount += 2;
+    }
+
 }
 
 template <typename T>
@@ -55,6 +68,22 @@ static void testOne() {
     runTest<T>({{false, 1}});
 }
 
+template <typename T>
+static void testTwo() {
+    runTest<T>({
+        {false, 1},
+        {true, 1}
+        });
+}
+
+template <typename T>
+static void testThree() {
+    runTest<T>({
+        {true, 1},
+        {true, 1},
+        {true, 1}
+        });
+}
 static void doTest() {
     testOne<BitDelay>();
     testOne<BitDelay2>();
@@ -65,7 +94,43 @@ void testBitDelayPartTwo() {
 }
 
 #if 1
+
+static void foo() {
+    BitDelay delay;
+    delay.setMaxDelaySamples(100);
+
+    delay.process(true, 0);
+    delay.process(true, 0);
+    delay.process(true, 0);
+
+    // param2:
+
+    // 0 -> false
+    // 1 -> true
+    // 2 -> true
+    // 3 -> true
+    // 4 -> false
+    // first output on first poll at 1
+    bool a = delay.process(false, 1);
+    SQINFO("a = %d", a);
+
+    // 0 -> false
+    // 1 -> false
+    // 2 -> true
+    // 3 -> true
+    // 4 -> true
+    // 5 -> false
+    // second output on second poll at 3
+    
+    a = delay.process(false, 5);
+    SQINFO("a = %d", a);
+
+}
+
+
+
 void testFirst() {
-    testOne<BitDelay>();
+   testThree<BitDelay>();
+   //foo();
 }
 #endif
