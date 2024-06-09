069db8ef4a2550c59783e40f6a661a394c3b80e5
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-06-09 16:53:48 -0700

    -

--- tests/testChordRecognizer.cpp
+++ tests/testChordRecognizer.cpp
@@ -35,13 +35,25 @@ static void testDMajorRecognized() {
     assert(std::get<1>(result) == MidiNote::D);
 }
 
+static void testCMajorOctaveRecognized() {
+    ChordRecognizer ch;
+    const int chord[] = { MidiNote::C+12, MidiNote::E+12, MidiNote::G+12, -1 };
+    auto const result = ch.recognize(chord);
+    ChordRecognizer::Type t = std::get<0>(result);
+    assert(t == ChordRecognizer::Type::MajorTriad);
+    assert(std::get<1>(result) == MidiNote::C);
+}
+
 void testChordRecognizer() {
     testCanCreate();
     testJunkNotRecognized();
     testCMajorRecognized();
     testDMajorRecognized();
+    testCMajorOctaveRecognized();
 }
 
+#if 0
 void testFirst() {
-    testDMajorRecognized();
+    testCMajorOctaveRecognized();
 }
+#endif
--- util/ChordRecognizer.cpp
+++ util/ChordRecognizer.cpp
@@ -10,13 +10,13 @@ std::tuple<ChordRecognizer::Type, int> ChordRecognizer::recognize(const int* cho
     int normalizedChord[16];
     int i;
     for (i=0; chord[i] >=0; ++i) {
-        normalizedChord[i] = chord[i] - base;
+        int note = chord[i] - base; // normalize to base
+        note = note % 12;           // normalize to octave
+        normalizedChord[i] = note;
     }
     normalizedChord[i] = -1;
-    // assert(false);
-    // return std::make_tuple(Type::Unrecognized, MidiNote::C);
     const auto t = recognizeType(normalizedChord);
-    return std::make_tuple(t, base);
+    return std::make_tuple(t, base % 12);
 
 }
 
