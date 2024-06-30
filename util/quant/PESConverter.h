22851412803bd30c0ae707aed3c133d00e78dfc0
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-06-30 16:42:26 -0700

    -

--- util/quant/PESConverter.h
+++ util/quant/PESConverter.h
@@ -16,9 +16,53 @@ public:
         int keyRoot = 0;
     };
 
-    static PESOutput convertToPES(const Input& input);
+    static PESOutput convertToPES(Input& input);
     static void outputPES(Output& output, const PESOutput& params);
 };
+
+inline PESConverter::PESOutput PESConverter::convertToPES(Input& input) {
+      if (input.channels < 1) {
+        PESOutput ret;   // unconnected
+        ret.valid = true;
+    }
+    if (input.channels < 12) {
+        // wrong number of channels - error
+       // TBase::lights[PES_INVALID_LIGHT].value = 8;
+        return PESOutput();
+    }
+    Scale::Role roles[13];
+    bool haveAddedRoot = false;
+    for (int i = 0; i < 12; ++i) {
+        float v = input.getVoltage(i);
+        Scale::Role role;
+        if (v < 4) {
+            role = Scale::Role::NotInScale;
+        } else if (v < 9) {
+            role = Scale::Role::InScale;
+        } else {
+            // only add the first root. Some modules send all note as 10V.
+            role = haveAddedRoot ? Scale::Role::InScale : Scale::Role::Root;
+            haveAddedRoot = true;
+        }
+        roles[i] = role;
+    }
+    roles[12] = Scale::Role::End;
+    PESOutput returnValue;
+
+    const auto scaleConverted = Scale::convert(roles, false);
+    if (std::get<0>(scaleConverted) == false) {
+        //TBase::lights[PES_INVALID_LIGHT].value = 8;
+        returnValue.valid = false;
+    } else {
+       // TBase::lights[PES_INVALID_LIGHT].value = 0;
+       returnValue.valid = true;
+        //TBase::params[KEY_PARAM].value = std::get<1>(scaleConverted).get();
+        returnValue.keyRoot = std::get<1>(scaleConverted).get();
+       // TBase::params[MODE_PARAM].value = int(std::get<2>(scaleConverted));
+       returnValue.mode = std::get<2>(scaleConverted);
+    }
+    return returnValue;
+}
 
 inline void PESConverter::outputPES(Output& output, const PESOutput& params) {
     if (output.isConnected()) {
