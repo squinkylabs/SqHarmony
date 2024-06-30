3fd1c7a3f75b1d62b2b3b8612bfe5a618c7700f5
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-06-30 15:19:38 -0700

    -

--- tests/testPESConverter.cpp
+++ tests/testPESConverter.cpp
@@ -1,8 +1,42 @@
 
+#include "PESConverter.h"
 #include "asserts.h"
 
-#include "PESConverter.h"
+static void outputCMajor() {
+    TestComposite comp;
+    auto outputToTest = comp.outputs[2];
+    outputToTest.channels = 1;  // connect it.
+
+    PESConverter::PESOutput params;
+    params.keyRoot = 0;  // C
+    params.mode = Scale::Scales::Major;
+    PESConverter::outputPES(outputToTest, params);
+    assertEQ(outputToTest.getChannels(), 12);
+
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(1), 0);   // C#
+    assertEQ(outputToTest.getVoltage(2), 8);   // D
+
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+    assertEQ(outputToTest.getVoltage(0), 10);  // C
+
+    //   const auto note = MidiNote(MidiNote::C + int(std::round(TBase::params[KEY_PARAM].value)));
+    // const auto mode = Scale::Scales(int(std::round(TBase::params[MODE_PARAM].value)));
+}
 
 void testPESConverter() {
-    assert(false);
+    outputCMajor();
 }
+
+#if 1
+void testFirst() {
+    outputCMajor();
+}
+#endif
--- util/quant/PESConverter.h
+++ util/quant/PESConverter.h
@@ -1,6 +1,48 @@
 
 #pragma once
 
-class PESConverter {
+#include "Scale.h"
+#include "TestComposite.h"
 
+class PESConverter {
+public:
+    //   TBase::lights[PES_INVALID_LIGHT].value = 0;
+    //     TBase::params[KEY_PARAM].value = std::get<1>(scaleConverted).get();
+    //    TBase::params[MODE_PARAM].value = int(std::get<2>(scaleConverted));
+    class PESOutput {
+    public:
+        bool valid = false;
+        Scale::Scales mode = Scale::Scales::Major;
+        int keyRoot = 0;
+    };
+
+    static PESOutput convertToPES(const Input& input);
+    static void outputPES(Output& output, const PESOutput& params);
 };
+
+inline void PESConverter::outputPES(Output& output, const PESOutput& params) {
+    if (output.isConnected()) {
+        output.channels = 12;
+    }
+
+    const auto note = MidiNote(MidiNote::C + params.keyRoot);
+    const auto mode = params.mode;
+    const Scale::RoleArray roleArray = Scale::convert(note, mode);
+    for (int i = 0; i < 12; ++i) {
+        float v = 0;
+        switch (roleArray.data[i]) {
+            case Scale::Role::InScale:
+                v = 8;
+                break;
+            case Scale::Role::Root:
+                v = 10;
+                break;
+            case Scale::Role::NotInScale:
+            case Scale::Role::End:
+                break;
+            default:
+                assert(false);
+        }
+        output.setVoltage(v, i);
+    }
+}
