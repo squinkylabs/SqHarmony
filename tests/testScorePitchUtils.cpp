fa121ece4a0b99394389d987a16ebbde403013fb
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2024-07-04 12:02:06 -0700

    -

--- tests/testScorePitchUtils.cpp
+++ tests/testScorePitchUtils.cpp
@@ -7,31 +7,47 @@ static void test() {
     MidiNote mn(MidiNote::C);
     sc.set(mn, Scale::Scales::Major);
     //     static NotationNote getNotationNote(const Scale&, const MidiNote&, bool bassStaff);
-  //  static NotationNote getNotationNoteForce(const Scale&, const MidiNote&, bool bassStaff, bool useSharps);
-   ScorePitchUtils::getNotationNote(sc, mn, false);
-   ScorePitchUtils::getNotationNoteForce(sc, mn, false, true);
+    //  static NotationNote getNotationNoteForce(const Scale&, const MidiNote&, bool bassStaff, bool useSharps);
+    ScorePitchUtils::getNotationNote(sc, mn, false);
+    ScorePitchUtils::getNotationNoteForce(sc, mn, false, true);
 }
 
-static void testCMajor() {
+static void testCMajorC() {
     Scale sc;
-    MidiNote mnc(MidiNote::C);
-    MidiNote mncSharp(MidiNote::C + 1);
-    sc.set(mnc, Scale::Scales::Major);
+    MidiNote mnc(MidiNote::C3 + 12);
+    sc.set(MidiNote(MidiNote::C), Scale::Scales::Major);
 
+    // C should default to drawing as C natural
     auto x = ScorePitchUtils::getNotationNote(sc, mnc, false);
     assert(x._accidental == ScorePitchUtils::Accidental::none);
+    assertEQ(x._legerLine, -2);
+    assert(x._scaleNote.getAdjustment() == ScaleNote::RelativeAdjustment::none);
 
-    // Even when we try to "force" C to use sharps or flats it should.
+    //-----------------------------------
+    // Even when we try to "force" C to use sharps or flats it should stay natural
     x = ScorePitchUtils::getNotationNoteForce(sc, mnc, false, true);
     assert(x._accidental == ScorePitchUtils::Accidental::none);
+    assert(x._accidental == ScorePitchUtils::Accidental::none);
+    assertEQ(x._legerLine, -2);
+    assert(x._scaleNote.getAdjustment() == ScaleNote::RelativeAdjustment::none);
+
     x = ScorePitchUtils::getNotationNoteForce(sc, mnc, false, false);
     assert(x._accidental == ScorePitchUtils::Accidental::none);
+    assert(x._accidental == ScorePitchUtils::Accidental::none);
+    assertEQ(x._legerLine, -2);
+    assert(x._scaleNote.getAdjustment() == ScaleNote::RelativeAdjustment::none);
+}
+
+static void testCMajorCSharp() {
+    Scale sc;
+    MidiNote mncSharp(MidiNote::C3 + 12 + 1);
+    sc.set(MidiNote(MidiNote::C), Scale::Scales::Major);
 
     // This note is also D-. But C Major defaults to sharps
-    x = ScorePitchUtils::getNotationNote(sc, mncSharp, false);
+    auto x = ScorePitchUtils::getNotationNote(sc, mncSharp, false);
     assert(x._accidental == ScorePitchUtils::Accidental::sharp);
 
-     x = ScorePitchUtils::getNotationNoteForce(sc, mncSharp, false, true);
+    x = ScorePitchUtils::getNotationNoteForce(sc, mncSharp, false, true);
     assert(x._accidental == ScorePitchUtils::Accidental::sharp);
     x = ScorePitchUtils::getNotationNoteForce(sc, mncSharp, false, false);
     assert(x._accidental == ScorePitchUtils::Accidental::flat);
@@ -42,16 +58,15 @@ static void testCMinor() {
     // MidiNote mnc(MidiNote::C);
     // MidiNote mncSharp(MidiNote::C + 1);
     MidiNote mnEFlat(MidiNote::E - 1);
-    MidiNote mnE(MidiNote::E );
+    MidiNote mnE(MidiNote::E);
     sc.set(mnEFlat, Scale::Scales::Minor);
 
     // In C minor, E flat notated needs no accidental
-  auto x = ScorePitchUtils::getNotationNote(sc, mnEFlat, false);
-  assert(x._accidental == ScorePitchUtils::Accidental::none);
+    auto x = ScorePitchUtils::getNotationNote(sc, mnEFlat, false);
+    assert(x._accidental == ScorePitchUtils::Accidental::none);
 
-  x = ScorePitchUtils::getNotationNote(sc, mnE, false);
-  assert(x._accidental == ScorePitchUtils::Accidental::natural);
-    
+    x = ScorePitchUtils::getNotationNote(sc, mnE, false);
+    assert(x._accidental == ScorePitchUtils::Accidental::natural);
 }
 
 #if 0
@@ -72,7 +87,8 @@ static void  testCompare() {
 #endif
 void testScorePitchUtils() {
     test();
-    testCMajor();
+    testCMajorC();
+    testCMajorCSharp();
     testCMinor();
     // testCompare();
 }
@@ -80,7 +96,7 @@ void testScorePitchUtils() {
 #if 1
 void testFirst() {
     testScorePitchUtils();
-   // testCMinor();
-  // testCompare();
+    // testCMinor();
+    // testCompare();
 }
 #endif
