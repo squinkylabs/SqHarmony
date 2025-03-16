b2066676f9c654fa2245c0d275360184f9d61d8d
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2025-03-16 16:10:01 -0700

    -

--- tests/testMelodyGenerator.cpp
+++ tests/testMelodyGenerator.cpp
@@ -1,10 +1,91 @@
 
 #include "asserts.h"
 
+#include "MelodyGenerator.h"
+
+#include "NoteConvert.h"
+#include "Scale.h"
+
+static void testMelodyRowSize() {
+    MelodyRow r;
+    r.setSize(2);
+    assertEQ(r._getSize(), 2);
+} 
+
+static void testMelodyRowInit() {
+    MelodyRow r;
+    size_t size = 7;
+    Scale scale;
+
+    // test in c major
+    MidiNote base(MidiNote::C);
+    scale.set(base, Scale::Scales::Major);
+
+    {
+        // sanity check
+        ScaleNote scaleNote;
+        NoteConvert::m2s(scaleNote, scale, base);
+        assertEQ(scaleNote.getDegree(), 0);
+    }
+
+    r.init(size, scale);
+
+    assertEQ(r._getSize(), size);
+
+    for (unsigned i = 0; i < size; ++i) {
+        ScaleNote scaleNote;
+        const MidiNote midiNote = r.getNote(i);
+        NoteConvert::m2s(scaleNote, scale, midiNote);       
+        assertEQ(scaleNote.getDegree(), 0);
+        assertEQ(scaleNote.getOctave(), 4);
+    }
+}
+
+static void testMelodyRowEqual() {
+    MelodyRow r;
+    size_t size = 3;
+    Scale scale;
+
+    // test in E phrygian
+    MidiNote base(MidiNote::E);
+    scale.set(base, Scale::Scales::Phrygian);
+
+    {
+        // sanity check
+        ScaleNote scaleNote;
+        NoteConvert::m2s(scaleNote, scale, base);
+        assertEQ(scaleNote.getDegree(), 0);
+    }
+
+    r.init(size, scale);
+    MelodyRow r2 = r;
+    assert(r == r2);
+
+
+    Scale scale2;
+    MidiNote base2(MidiNote::F);
+    scale2.set(base2, Scale::Scales::Phrygian);
+    r2.init(size, scale2);
+    assert(r != r2);
+
+    r2 = r;
+    r2.setSize(size + 1);
+    assert(r != r2);
+
+}
+
+static void testMelodyRow() {
+    testMelodyRowSize();
+    testMelodyRowInit();
+    testMelodyRowEqual();
+}
+
 void testMelodyGenerator() {
-   assert(false); 
+    testMelodyRow();
 }
 
 void testFirst() {
-    testMelodyGenerator();
+   testMelodyGenerator();
+   //testMelodyRowInit();
+
 }
--- util/MelodyGenerator.h
+++ util/MelodyGenerator.h
@@ -1,10 +1,56 @@
+#include <vector>
 
+#include "MidiNote.h"
+#include "Scale.h"
 
 class MelodyRow {
+public:
+    /**
+     * @brief initialize entire row to root of scale
+     *
+     * @param size
+     * @param scale
+     */
+    void init(size_t size, const Scale& scale);
+    bool operator==(const MelodyRow& other) const;
+    bool operator!=(const MelodyRow& other) const { return !(*this == other); }
 
+
+    void setSize(unsigned);
+    size_t _getSize() const { return notes.size(); }
+
+    const MidiNote& getNote(size_t index) { return notes[index]; }
+
+private:
+    std::vector<MidiNote> notes;
 };
 
+inline void MelodyRow::init(size_t size, const Scale& scale) {
+    setSize(size);
+
+    // get the root of the scale, normalize to middle octave
+    // TODO: this gets relative!!!
+   // const int semi = scale.degreeToSemitone(0);
+ //   const MidiNote root(MidiNote::MiddleC + semi);
+
+    ScaleNote scaleNote(0, 4);          // root, middle octave
+    assert(scaleNote.getOctave() == 4);         // we want to init in middle octave.
+    const MidiNote root = scale.s2m(scaleNote);
+    for (int i = 0; i < size; ++i) {
+        notes[i] = root;
+    }
+}
+
+inline void MelodyRow::setSize(unsigned size) {
+    notes.clear();
+    notes.resize(size);
+}
+
+inline bool MelodyRow::operator==(const MelodyRow& other) const {
+    return notes == other.notes;
+}
 
 class MelodyGenerator {
-
+public:
+    static void changeOneNote(MelodyRow& row, size_t index, int stepsToChange);
 };
